20be29c89cc7afb7d193500a3c5c75462ec90e1e
Dustin Tatum <dtatum0826@gmail.com>
2024-03-06 18:24:25 -0600

    Removed secret

--- backend/server.mjs
+++ backend/server.mjs
@@ -2,22 +2,24 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import todoRoutes from './routes/todos.mjs';
+ 
 import dotenv from 'dotenv';
 dotenv.config();
 
 
-
-
 const app = express();
-const port = 5000 || 9000;// Use environment variable or default
+const port = 5000|| 9000;// Use environment variable or default
 
 app.use(cors());
 app.use(express.json()); 
 app.use('/api/todos', todoRoutes);
 
+const connectionString = import.meta
 
+
+console.log(connectionString)
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://dtatum0826:UaL91q202DfKLsf6@cluster0.uyhccp0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(import.meta.env.VITE_MONGODB_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
